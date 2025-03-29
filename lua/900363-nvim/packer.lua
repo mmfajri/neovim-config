677f9aaeb3db3392e0277ea79fd3ec4b09785814
mmfajri <mmathlailfajri@gmail.com>
2025-03-29 15:13:38 +0800

    MyVimVersion : Install and Config TokyoNight ColorScheme and LuaLineToggle

--- lua/900363-nvim/packer.lua
+++ lua/900363-nvim/packer.lua
@@ -1,21 +1,34 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-	--use 'xiyaowong/transparent.nvim'
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use({
-		'rose-pine/neovim',
-		as = 'rose-pine',
-		config = function()
-			vim.cmd('colorscheme rose-pine')
-		end
-	})
+    --Download The Nerd Icon Later
+    --https://www.nerdfonts.com/
+    --
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    --use 'xiyaowong/transparent.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+    -- use({
+    -- 	'rose-pine/neovim',
+    -- 	as = 'rose-pine',
+    -- 	config = function()
+    -- 		vim.cmd('colorscheme rose-pine')
+    -- 	end
+    -- })
+    use({
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {}
+    })
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
     use('tpope/vim-fugitive')
     use {
         'nvim-treesitter/nvim-treesitter',
@@ -24,14 +37,14 @@ return require('packer').startup(function(use)
             ts_update()
         end,
     }
-	use 'mbbill/undotree'
-	use({'hrsh7th/nvim-cmp'})
-	use({'hrsh7th/cmp-nvim-lsp'})
-	use {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	}
+    use 'mbbill/undotree'
+    use({ 'hrsh7th/nvim-cmp' })
+    use({ 'hrsh7th/cmp-nvim-lsp' })
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
     use({
         "kdheepak/lazygit.nvim",
         -- optional for floating window border decoration
@@ -39,7 +52,7 @@ return require('packer').startup(function(use)
             "nvim-lua/plenary.nvim",
         },
         config = function()
-            vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>", { noremap = true, silent = true})
+            vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>", { noremap = true, silent = true })
         end,
     })
     use {
@@ -48,5 +61,4 @@ return require('packer').startup(function(use)
             require('Comment').setup()
         end
     }
-
 end)
