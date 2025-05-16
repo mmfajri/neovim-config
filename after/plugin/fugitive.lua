7073753cd8c82af025a1d271098a55bd18596d2f
mmfajri <mmathlaiil.fajri@bni.co.id>
2025-05-16 09:49:55 +0700

    KickStartConfig : Initialized

--- after/plugin/fugitive.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set("n", "<leader>gstat", vim.cmd.Git)
-vim.keymap.set("n", "<leader>ggraph", ":Git log --graph --oneline --decorate --all<CR>", { noremap = true, silent = true })
--- lua/900363-nvim/init.lua
+++
@@ -1,8 +0,0 @@
---THIS IS THE FILE NAME INSIDE THE LUA DIRECTORY
-require("900363-nvim.remap")
-require("900363-nvim.packer")
-require("900363-nvim.set")
---require("900363-nvim.transparancy")
-
---THIS IS GONNA ADD TO CHANGE NEXT TIME 
---This is gonna diff testing using lazygit 
