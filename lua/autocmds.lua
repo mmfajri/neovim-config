54ce8a03df0d99c77ea79c011897cafbbde0c3b9
mmfajri <mmathlailfajri@gmail.com>
2025-06-04 02:14:52 +0700

    KickStartConfig : Fixing the lazygit colorscheme

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,42 +1,35 @@
-local lazygit_group = vim.api.nvim_create_augroup('LazyGitFix', { clear = true })
+local lazygit_group = vim.api.nvim_create_augroup('LazyGitColors', { clear = true })
 
--- 1. Fix LazyGit colors without leaking to Neovim
+-- When opening LazyGit
 vim.api.nvim_create_autocmd('TermOpen', {
   group = lazygit_group,
   pattern = 'term://*lazygit*',
   callback = function()
-    -- Save current settings
-    vim.g.pre_lazygit_settings = {
-      termguicolors = vim.o.termguicolors,
-      colorscheme = vim.g.colors_name,
-    }
+    -- Save current colorscheme
+    vim.g.pre_lazygit_colorscheme = vim.g.colors_name
 
-    -- Force terminal-friendly colors
+    -- Apply terminal-friendly colorscheme
+    vim.cmd 'colorscheme elflord' -- or darkblue/industry/elflord/desert
+
+    -- Force 256-color mode for consistency
     vim.opt_local.termguicolors = false
-    vim.cmd 'highlight! TermCursorNC ctermbg=8 ctermfg=15'
-    vim.cmd 'highlight! TermCursor ctermbg=8 ctermfg=15'
   end,
 })
 
--- 2. Restore Neovim + fix todo-comments after exit
+-- When closing terminal
 vim.api.nvim_create_autocmd('TermClose', {
   group = lazygit_group,
   pattern = 'term://*lazygit*',
   callback = function()
-    if vim.g.pre_lazygit_settings then
-      -- Restore original colors
-      vim.opt.termguicolors = vim.g.pre_lazygit_settings.termguicolors
-      vim.cmd('colorscheme ' .. vim.g.pre_lazygit_settings.colorscheme)
+    if vim.g.pre_lazygit_colorscheme then
+      -- Restore original colorscheme
+      vim.cmd('colorscheme ' .. vim.g.pre_lazygit_colorscheme)
+      vim.opt.termguicolors = true
 
-      -- Force reload todo-comments
+      -- Refresh plugins
       if package.loaded['todo-comments'] then
-        package.loaded['todo-comments'] = nil
         require('todo-comments').setup()
       end
-
-      -- Refresh UI (critical for plugin stability)
-      vim.cmd 'checktime'
-      vim.cmd 'mode' -- Force redraw
     end
   end,
 })
