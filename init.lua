6e40210ef0dea5bf96b2150198ed9027a54615c3
mmfajri <mmathlailfajri@gmail.com>
2025-06-09 17:49:54 +0700

    KickStartConfig : disabled custom theme for lazygit

--- init.lua
+++ init.lua
@@ -107,6 +107,6 @@ require 'lazy-plugins'
 
 require 'global-function.transparent'
 
-require 'autocmds'
+-- require 'autocmds'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
