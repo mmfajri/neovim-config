627c19a23620c81566f45e77496a0f9a7beb1d1f
mmfajri <mmathlailfajri@gmail.com>
2025-06-17 07:01:56 +0700

    KickStartConfig : My Current Roslyn and rzls config

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -58,6 +58,7 @@ require('lazy').setup({
   require 'custom/plugins/spectre',
 
   require 'custom/plugins/roslyn',
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   --
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
