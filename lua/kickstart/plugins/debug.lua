045f9ddc0a708bd25ce93014d6b99c0ff49babb5
mmfajri <mmathlaiil.fajri@bni.co.id>
2025-10-01 17:33:07 +0700

    refactor : debug config

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -23,6 +23,13 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
+
+    'mxsdev/nvim-dap-vscode-js',
+    {
+      'microsoft/vscode-js-debug',
+      version = '1.x',
+      build = 'npm i && npm run compile vsDebugServerBundle && mv dist out',
+    },
   },
   keys = {
     -- Basic debugging keymaps, feel free to change to your liking!
@@ -98,6 +105,67 @@ return {
       },
     }
 
+    --config Javascript/Typescript Debug
+    require('dap-vscode-js').setup {
+      debugger_path = vim.fn.stdpath 'data' .. '/lazy/vscode-js-debug',
+      adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+    }
+    for _, language in ipairs { 'typescript', 'javascript', 'svelte' } do
+      require('dap').configurations[language] = {
+        -- attach to a node process that has been started with
+        -- `--inspect` for longrunning tasks or `--inspect-brk` for short tasks
+        -- npm script -> `node --inspect-brk ./node_modules/.bin/vite dev`
+        {
+          -- use nvim-dap-vscode-js's pwa-node debug adapter
+          type = 'pwa-node',
+          -- attach to an already running node process with --inspect flag
+          -- default port: 9222
+          request = 'attach',
+          -- allows us to pick the process using a picker
+          processId = require('dap.utils').pick_process,
+          -- name of the debug action you have to select for this config
+          name = 'Attach debugger to existing `node --inspect` process',
+          -- for compiled languages like TypeScript or Svelte.js
+          sourceMaps = true,
+          -- resolve source maps in nested locations while ignoring node_modules
+          resolveSourceMapLocations = {
+            '${workspaceFolder}/**',
+            '!**/node_modules/**',
+          },
+          -- path to src in vite based projects (and most other projects as well)
+          cwd = '${workspaceFolder}/src',
+          -- we don't want to debug code inside node_modules, so skip it!
+          skipFiles = { '${workspaceFolder}/node_modules/**/*.js' },
+        },
+        {
+          type = 'pwa-chrome',
+          name = 'Launch Chrome to debug client',
+          request = 'launch',
+          url = 'http://localhost:5173',
+          sourceMaps = true,
+          protocol = 'inspector',
+          port = 9222,
+          webRoot = '${workspaceFolder}/src',
+          -- skip files from vite's hmr
+          skipFiles = { '**/node_modules/**/*', '**/@vite/*', '**/src/client/*', '**/src/*' },
+        },
+        -- only if language is javascript, offer this debug action
+        language == 'javascript'
+            and {
+              -- use nvim-dap-vscode-js's pwa-node debug adapter
+              type = 'pwa-node',
+              -- launch a new process to attach the debugger to
+              request = 'launch',
+              -- name of the debug action you have to select for this config
+              name = 'Launch file in new node process',
+              -- launch current file
+              program = '${file}',
+              cwd = '${workspaceFolder}',
+            }
+          or nil,
+      }
+    end
+
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
     dapui.setup {
@@ -121,16 +189,16 @@ return {
     }
 
     -- Change breakpoint icons
-    -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
-    -- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
-    -- local breakpoint_icons = vim.g.have_nerd_font
-    --     and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
-    --   or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
-    -- for type, icon in pairs(breakpoint_icons) do
-    --   local tp = 'Dap' .. type
-    --   local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
-    --   vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
-    -- end
+    vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
+    vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
+    local breakpoint_icons = vim.g.have_nerd_font
+        and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
+      or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
+    for type, icon in pairs(breakpoint_icons) do
+      local tp = 'Dap' .. type
+      local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+      vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+    end
 
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
