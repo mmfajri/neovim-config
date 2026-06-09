374cba66cdcb7d01b5131f19fe4f9593c5f8c375
mmfajri <mmathlailfajri@gmail.com>
2026-06-09 18:50:48 +0700

    fix : fixing comment/uncomment line and add local parser for treesitter

--- lua/custom/plugins/numtostr.lua
+++ lua/custom/plugins/numtostr.lua
@@ -1,25 +1,51 @@
 return {
-  -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
   {
     'numToStr/Comment.nvim',
     opts = {
-      -- add any options here
-    },
-    config = function()
-      require('Comment').setup()
+      -- 1. Disable the default 'gcc' and 'gc' keymaps completely
+      mappings = {
+        basic = false,
+        extra = false,
+      },
 
-      vim.keymap.set(
-        'n',
-        '<leader>c',
-        "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>",
-        { desc = 'Comment the Line', noremap = true, silent = true }
-      )
-      vim.keymap.set(
-        'v',
-        '<leader>c',
-        "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-        { desc = 'Comment / Uncomment the line', noremap = true, silent = true }
-      )
+      -- 2. Prevent Comment.nvim from acting inside Netrw explorer buffers
+      ignore = function()
+        return vim.bo.filetype == 'netrw'
+      end,
+
+      -- 3. Keep your fixed C# fallback logic active
+      pre_hook = function(ctx)
+        if vim.bo.filetype == 'cs' or vim.bo.filetype == 'csharp' then
+          local U = require 'Comment.utils'
+          return ctx.ctype == U.ctype.linewise and '// %s' or '/* %s */'
+        end
+      end,
+    },
+    config = function(_, opts)
+      local comment = require 'Comment'
+      comment.setup(opts)
+
+      -- 4. Create your custom <leader>c keymaps (Excluding Netrw)
+      vim.api.nvim_create_autocmd('FileType', {
+        -- Apply to all files, but explicitly skip 'netrw'
+        pattern = '*',
+        callback = function(args)
+          if vim.bo[args.buf].filetype == 'netrw' then
+            return
+          end
+
+          -- Normal mode: <leader>c to toggle current line
+          vim.keymap.set('n', '<leader>c', function()
+            return vim.v.count == 0 and '<Plug>(comment_toggle_linewise_current)' or '<Plug>(comment_toggle_linewise_count)'
+          end, { expr = true, buffer = args.buf, desc = 'Toggle comment line' })
+
+          -- Visual mode: <leader>c to toggle selection
+          vim.keymap.set('x', '<leader>c', '<Plug>(comment_toggle_linewise_visual)', {
+            buffer = args.buf,
+            desc = 'Toggle comment selection',
+          })
+        end,
+      })
     end,
   },
 }
--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -7,25 +7,64 @@ return {
     config = function()
       local ts = require 'nvim-treesitter'
 
-      ts.setup {
-        install_dir = vim.fn.stdpath 'data' .. '/site',
+      local languages = {
+        { parser = 'git_config' },
+        { parser = 'yaml' },
+        { parser = 'bash', filetypes = { 'sh' } },
+        { parser = 'c' },
+        { parser = 'c_sharp', filetypes = { 'cs' } },
+        { parser = 'diff' },
+        { parser = 'html' },
+        { parser = 'lua' },
+        { parser = 'luadoc' },
+        { parser = 'markdown' },
+        { parser = 'markdown_inline' },
+        { parser = 'query' },
+        { parser = 'vim' },
+        { parser = 'vimdoc', filetypes = { 'help' } },
+        { parser = 'razor' },
+        { parser = 'svelte' },
+        { parser = 'typescript' },
+        { parser = 'javascript' },
+        { parser = 'css' },
+        { parser = 'java' },
+        { parser = 'go' },
       }
 
-      -- Auto start highlighting
+      local parsers = {}
+
+      for _, lang in ipairs(languages) do
+        table.insert(parsers, lang.parser)
+
+        if lang.filetypes then
+          for _, ft in ipairs(lang.filetypes) do
+            vim.treesitter.language.register(lang.parser, ft)
+          end
+        end
+      end
+
+      ts.setup {
+        install_dir = vim.fn.stdpath 'data' .. '/site',
+        ensure_installed = parsers,
+      }
+
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
-          pcall(vim.treesitter.start, args.buf)
-        end,
-      })
+          local ft = vim.bo[args.buf].filetype
 
-      -- Auto install missing parser
-      vim.api.nvim_create_autocmd('FileType', {
-        callback = function()
-          local ft = vim.bo.filetype
+          local ignore = {
+            fidget = true,
+            TelescopePrompt = true,
+            TelescopeResults = true,
+            ['blink-cmp-menu'] = true,
+            lazy = true,
+          }
 
-          if not vim.tbl_contains(ts.get_installed(), ft) then
-            ts.install { ft }
+          if ignore[ft] then
+            return
           end
+
+          pcall(vim.treesitter.start, args.buf)
         end,
       })
     end,
